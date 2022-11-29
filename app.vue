4a051b27eb4346e6e6db790ac55fde6c05726f7b
MrFootwork <pandau.ting@gmail.com>
2022-11-29 09:36:37 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This works awsome!</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	typescript: {
+		shim: false,
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/_colors.scss" as *;',
+				},
+			},
+		},
+	},
+})
